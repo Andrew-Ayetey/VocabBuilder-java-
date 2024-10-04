9a5fbcd0911dca464fdba96a9a597001e834829d
serious573 <evens208@umn.edu>
2024-10-03 21:09:56 -0500

    push working GUI

---
+++ csv-anki-app/src/main/java/com/andrew/csvreader/gui.java
@@ -0,0 +1,39 @@
+package com.andrew.csvreader;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+/**
+ * JavaFX App
+ */
+public class gui extends Application {
+
+    private static Scene scene;
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        scene = new Scene(loadFXML("primary"), 640, 480);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    static void setRoot(String fxml) throws IOException {
+        scene.setRoot(loadFXML(fxml));
+    }
+
+    private static Parent loadFXML(String fxml) throws IOException {
+        var path = gui.class.getResource(fxml + ".fxml");
+        FXMLLoader fxmlLoader = new FXMLLoader(path);
+        return fxmlLoader.load();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
